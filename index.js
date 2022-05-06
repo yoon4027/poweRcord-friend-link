3acd7da967ba26f75a07cf459703798272e940a1
nick <69434290+lelboatz@users.noreply.github.com>
2022-05-05 22:47:56 -0400

    ability to view and delete links

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const { Plugin } = require("powercord/entities");
 const { getModule } = require("powercord/webpack");
 const { createFriendInvite } = getModule(["createFriendInvite"], false);
+const { getAllFriendInvites } = getModule(["getAllFriendInvites"], false);
+const { revokeFriendInvites } = getModule(["revokeFriendInvites"], false);
 class FriendLink extends Plugin {
   startPlugin() {
     powercord.api.commands.registerCommand({
@@ -10,6 +12,20 @@ class FriendLink extends Plugin {
       description: "Generates a friend link for your account.",
       usage: "{c}",
       executor: this.onCommand.bind(this),
+    });
+
+    powercord.api.commands.registerCommand({
+      command: 'viewlinks',
+      description: "Views all your friend links.",
+      usage: "{c}",
+      executor: this.onViewLinks.bind(this),
+    });
+
+    powercord.api.commands.registerCommand({
+      command: "deletelinks",
+      description: "Deletes all of your friend links.",
+      usage: "{c} <link id>",
+      executor: this.onDeleteLink.bind(this),
     });
   }
 
@@ -32,6 +48,45 @@ class FriendLink extends Plugin {
       };
     }
   }
+
+  async onViewLinks() {
+    try {
+      const invites = await getAllFriendInvites();
+      if (invites.length === 0) {
+        return {
+          send: false,
+          result: "You have no friend links.",
+        };
+      }
+      return {
+        send: false,
+        result: invites.map(invite => `https://discord.gg/${invite.code}`).join("\n"),
+      };
+    } catch (e) {
+      this.log(e);
+      return {
+        send: false,
+        result: "Failed to get your links.",
+      };
+    }
+  }
+
+  async onDeleteLink() {
+    try {
+      await revokeFriendInvites();
+      return {
+        send: false,
+        result: "Successfully deleted all of your links.",
+      };
+    } catch (e) {
+      this.log(e);
+      return {
+        send: false,
+        result: "Failed to delete your links.",
+      };
+    }
+  }
+
 }
 
 module.exports = FriendLink;
